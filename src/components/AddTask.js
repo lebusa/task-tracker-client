8f14b09087c94b8e0b16d5126c1b808d792c9527
Motebang Lebusa <teby4real@yahoo.com>
2021-03-25 19:59:28 +0200

    added form state setting funcs

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,34 @@
+import { useState } from "react";
+
 const AddTask = () => {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [reminder, setReminder] = useState(false);
+
   return (
     <form className="add-form">
       <div className="form-control">
-        <label>Add Task</label>
-        <input type="text" placeholder="Add a task" />
+        <label>Task</label>
+        <input
+          type="text"
+          placeholder="Add task"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div className="form-control">
         <label>Add day & time</label>
-        <input type="text" placeholder="Add dat & time" />
+        <input type="text" placeholder="Add day & time" 
+          value={date}
+          onChange={(e) => setDate(e.target.value)}/>
       </div>
       <div className="form-control form-control-check">
         <label>Set reminder</label>
-        <input type="checkbox" />
+        <input type="checkbox" 
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}/>
       </div>
-      <input type="submit" value="Save Task" className="btn btn-block"  />
+      <input type="submit" value="Save Task" className="btn btn-block" />
     </form>
   );
 };
