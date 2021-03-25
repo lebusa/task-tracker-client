54bdac2a46c73df37c149ecdc571c2c145ddb753
Motebang Lebusa <teby4real@yahoo.com>
2021-03-25 19:03:30 +0200

    task reminder toggling resonds on ui ok

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,22 @@
-import  { FaTrashAlt } from 'react-icons/fa';
+import { FaTrashAlt } from "react-icons/fa";
 
-const Task = ({task, onDelete, toggleReminder}) => {
-    return (
-        <div className="task" onDoubleClick={() => toggleReminder(task.id)}>
-            <h3>
-                {task.title}
-                <FaTrashAlt className="task-remove" onClick={ () => onDelete(task.id)} />
-            </h3>
-            <p>{task.scheduledFor}</p>
-        </div>
-    )
-}
+const Task = ({ task, onDelete, toggleReminder }) => {
+  const dt = new Date(task.scheduledFor).toLocaleDateString();
+  const tm = new Date(task.scheduledFor).toLocaleTimeString();
+  return (
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => toggleReminder(task.id)}
+    >
+      <h3>
+        {task.title}
+        <FaTrashAlt className="task-remove" onClick={() => onDelete(task.id)} />
+      </h3>
+      <p>
+        {dt} @ {tm}
+      </p>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
