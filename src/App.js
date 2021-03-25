d44e102e5b25f4f68da899c45abf7d9acb012fe9
Motebang Lebusa <teby4real@yahoo.com>
2021-03-25 22:37:30 +0200

    at last got fetch workin via http & new URL

--- src/App.js
+++ src/App.js
@@ -1,55 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 import Footer from "./components/Footer";
+// import axios from 'axios';
+
+const API_URL = new URL("http://localhost:4000/api/tasks");
 
 function App() {
   const [showAddTaskForm, setShowAddTaskForm] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      title: "'na ke tla pele",
-      scheduledFor: "2021-03-15T12:23:44.874Z",
-      reminder: false,
-      id: 1616687637698,
-    },
-    {
-      title: "'na kea tsamaea",
-      scheduledFor: "2021-03-20T12:45:44.874Z",
-      reminder: true,
-      id: 1616687637694,
-    },
-    {
-      title: "palama koloi re ee skolong",
-      scheduledFor: "2021-03-28T08:00:00.000Z",
-      reminder: true,
-      id: 1616687637594,
-    },
-    {
-      title: "u ntlele le ntho tse monate",
-      scheduledFor: "2021-03-29T03:30:00.365Z",
-      reminder: true,
-      id: 1616687637674,
-    },
-    {
-      title: "kea sekolong, ke palama schoobase",
-      scheduledFor: "2021-03-26T07:45:44.874Z",
-      reminder: false,
-      id: 1616687637697,
-    },
-    {
-      title: "'na ke popa ntha tse monate",
-      scheduledFor: "2021-03-25T16:45:44.874Z",
-      reminder: true,
-      id: 1616687637696,
-    },
-    {
-      title: "Lintle o setse Botha-Bothe",
-      scheduledFor: "2021-03-10T16:04:44.874Z",
-      reminder: false,
-      id: 1616687637695,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
+
+
+  // fetch tasks from a tasks server api
+  useEffect(() => {
+    const fetchTasks = async () => {
+      
+      const options = {
+        mode: 'cors',
+      }
+
+      const res = await fetch(API_URL, options);
+      console.log(res)
+      const data = await res.json();
+      console.log(data)
+    }
+    fetchTasks()
+    .catch(err => {
+      console.log('Liphoso', err);
+      return;
+    });
+  }, []);
 
   // toggle task add form
   const toggledAddTask = () => {
