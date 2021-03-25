285dc6f7597680eec7a953a7cd13c9087cf25a19
Motebang Lebusa <teby4real@yahoo.com>
2021-03-25 14:28:16 +0200

    used state to create tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,36 @@
-const tasks = [
-    {
-        title: "'na ke tla pele",
-        scheduledFor: "2021-03-15T12:23:44.874Z",
-        reminder: false
-    },
-    {
-        title: "'na kea tsamaea",
-        scheduledFor: "2021-03-20T12:45:44.874Z",
-        reminder: true
-    },
-    {
-        title: "'na ke popa ntha tse monate",
-        scheduledFor: "2021-03-25T16:45:44.874Z",
-        reminder: true
-    },
-    {
-        title: "Lintle o setse Botha-Bothe",
-        scheduledFor: "2021-03-10T16:04:44.874Z",
-        reminder: false
-    },
-];
+import { useState } from 'react';
+
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState(
+        [
+            {
+                title: "'na ke tla pele",
+                scheduledFor: "2021-03-15T12:23:44.874Z",
+                reminder: false
+            },
+            {
+                title: "'na kea tsamaea",
+                scheduledFor: "2021-03-20T12:45:44.874Z",
+                reminder: true
+            },
+            {
+                title: "kea sekolong, ke palama schoobase",
+                scheduledFor: "2021-03-26T07:45:44.874Z",
+                reminder: true
+            },
+            {
+                title: "'na ke popa ntha tse monate",
+                scheduledFor: "2021-03-25T16:45:44.874Z",
+                reminder: true
+            },
+            {
+                title: "Lintle o setse Botha-Bothe",
+                scheduledFor: "2021-03-10T16:04:44.874Z",
+                reminder: false
+            },
+        ]
+    );
     return (
         <>
           {tasks.map((task,i) => <p key={i}>{task.title}</p>)}  
