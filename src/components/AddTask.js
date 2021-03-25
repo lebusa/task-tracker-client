cf32ba8fb3412b5b181caf755b0698b6fa933afe
Motebang Lebusa <teby4real@yahoo.com>
2021-03-25 19:44:40 +0200

    added further form features

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,21 @@
 const AddTask = () => {
-    return (
-        <form className="add-form">
-            <div className="form-control">
-            <label htmlFor="">Add Task</label>
-            <input type="text" placeholder="Add a task" />
-            </div>
-        </form>
-    )
-}
+  return (
+    <form className="add-form">
+      <div className="form-control">
+        <label>Add Task</label>
+        <input type="text" placeholder="Add a task" />
+      </div>
+      <div className="form-control">
+        <label>Add day & time</label>
+        <input type="text" placeholder="Add dat & time" />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set reminder</label>
+        <input type="checkbox" />
+      </div>
+      <input type="submit" value="Save Task" className="btn btn-block"  />
+    </form>
+  );
+};
 
-export default AddTask
+export default AddTask;
