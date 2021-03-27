10e32c37bb9e8d18520d952eee3c69a652467b99
Motebang Lebusa <teby4real@yahoo.com>
2021-03-27 21:40:32 +0200

    moved credits to About pg frm footer

--- src/components/About.js
+++ src/components/About.js
@@ -10,11 +10,25 @@ const About = () => {
     <div>
       <h5>Version: 1.0.0</h5>
       
-      <h6>Developer: M. Lebusa</h6>
-
+      <h6>Developer: Motebang Lebusa</h6>
+      
       <Link to="/" className="link">
         Tasks
       </Link>
+      
+
+      <p>Credits:{" "}
+        <Link
+          to="https://www.youtube.com/watch?v=w7ejDZ8SWv8"
+          rel="noreferrer"
+          target="_blank"
+          className="link"
+        >
+          Traversy Media React Crash Course 2021
+        </Link>
+      </p>
+
+     
     </div>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,7 @@ const Footer = (props) => {
     <footer className="footer">
       <div className="col"></div>
 
-      <small>
-        Credits:{" "}
-        <Link
-          to="https://www.youtube.com/watch?v=w7ejDZ8SWv8"
-          rel="noreferrer"
-          target="_blank"
-          className="link"
-        >
-          Traversy Media
-        </Link>
-      </small>
+      
 
       <div>Copyright &copy; 2021. Lebusa, M.</div>
       <div>
