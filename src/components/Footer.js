041f2cdc7939ff0b8add6574ab51feaad9b819bf
Motebang Lebusa <teby4real@yahoo.com>
2021-03-27 15:56:45 +0200

    fixed sec iss frm _blank

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = props => {
     return (
         <footer className="footer">
-            <small>Credits: <a href="https://www.youtube.com/watch?v=w7ejDZ8SWv8" target="_blank">Traversy Media</a></small>
+            <small>Credits: <a href="https://www.youtube.com/watch?v=w7ejDZ8SWv8"  rel="noreferrer" target="_blank">Traversy Media</a></small>
             <br />
             <small>Copyright &copy; 2021. Lebusa, M.</small> 
         </footer>
