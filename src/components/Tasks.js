d7955f7db98680e10c3161c4ee8f28a3a186c860
Motebang Lebusa <teby4real@yahoo.com>
2021-03-27 20:38:53 +0200

    format files

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,20 +1,24 @@
 import { useState, useEffect } from "react";
-import Task from './Task';
+import Task from "./Task";
 
-const Tasks = ({tasks, onDelete, toggleReminder}) => {
-  
+const Tasks = ({ tasks, onDelete, toggleReminder }) => {
   useEffect(() => {
     // set doc title
     document.title = "Tasks | Tasks Tracker";
   }, []);
 
-    return (
-        <>
-          {tasks.map((task,i) => 
-          <Task key={i} task={task} onDelete={onDelete}  toggleReminder={toggleReminder} />
-          )}  
-        </>
-    )
-}
+  return (
+    <>
+      {tasks.map((task, i) => (
+        <Task
+          key={i}
+          task={task}
+          onDelete={onDelete}
+          toggleReminder={toggleReminder}
+        />
+      ))}
+    </>
+  );
+};
 
-export default Tasks
+export default Tasks;
